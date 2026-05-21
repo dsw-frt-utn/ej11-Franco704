420c21e830483c24985a826473d897d95ec6863f
Franco704 <franmartinez704@gmail.com>
2026-05-21 11:01:46 -0300

    Ejemplos List y Dictionary completados

--- Dsw2026Ej11/Tests/Ejemplos.cs
+++ Dsw2026Ej11/Tests/Ejemplos.cs
@@ -1,4 +1,7 @@
-﻿namespace Dsw2026Ej11.Tests;
+﻿using Dsw2026Ej11.Collections;
+using Dsw2026Ej11.Domain;
+
+namespace Dsw2026Ej11.Tests;
 
 
 internal class Ejemplos
@@ -11,6 +14,52 @@ internal class Ejemplos
     //Eliminar el primer elemento de la lista y listar por consola los alumnos
     public static void EjemploList()
     {
+        Console.WriteLine("Ejemplo List");
+        CasoList casoList = new CasoList();
+
+        Alumno a1 = new Alumno(1, "Franco", 7.0);
+        Alumno a2 = new Alumno(2, "Celeste", 8.0);
+        Alumno a3 = new Alumno(3, "Pedro", 9.0);
+        
+        casoList.AgregarAlumno(a1);
+        casoList.AgregarAlumno(a2);
+        casoList.AgregarAlumno(a3);
+
+        Console.WriteLine("Contenido de la lista: ");
+        foreach (var Alumno in casoList.ListarAlumnos())
+        {
+            Console.WriteLine($"- {Alumno.Id}: {Alumno.Nombre} | Promedio {Alumno.Promedio}");
+        }
+        
+        
+        Alumno? busqueda = casoList.BuscarAlumno("Franco");
+        Console.WriteLine("Buscando alumno que existe");
+        if (busqueda != null)
+        {
+            Console.WriteLine($"Alumno encontrado: {busqueda.Id}: {busqueda.Nombre} | Promedio {busqueda.Promedio}");
+        }
+
+        Alumno? busqueda2 = casoList.BuscarAlumno("Salomon");
+        Console.WriteLine("Buscando alumno que no existe");
+            if (busqueda2 == null)
+            {
+                Console.WriteLine("No existe");
+            }
+
+        Console.WriteLine("Eliminando alumno y mostrando");    
+        casoList.EliminarAlumno(a2);
+        foreach (var Alumno in casoList.ListarAlumnos())
+        {
+            Console.WriteLine($"{Alumno.Id} {Alumno.Nombre} | Promedio {Alumno.Promedio}");
+        }
+
+        Console.WriteLine("Eliminando primer alumno y mostrando");
+        casoList.EliminarAlumno(0);
+        foreach (var Alumno in casoList.ListarAlumnos())
+        {
+            Console.WriteLine($"{Alumno.Id} {Alumno.Nombre} | Promedio {Alumno.Promedio}");
+        }
+
 
     }
 
@@ -21,6 +70,44 @@ internal class Ejemplos
     //Eliminar un alumno por clave y listar por consola los alumnos
     public static void EjemploDictionary()
     {
+        Console.WriteLine("Ejemplo Dictionary");
+
+        CasoDictionary casoDictionary = new CasoDictionary();
+        
+        Alumno a1 = new Alumno(1, "Franco", 7.0);
+        Alumno a2 = new Alumno(2, "Celeste", 8.0);
+        Alumno a3 = new Alumno(3, "Pedro", 9.0);
+        
+        casoDictionary.AgregarAlumno(a1);
+        casoDictionary.AgregarAlumno(a2);
+        casoDictionary.AgregarAlumno(a3);
+        
+        Console.WriteLine("Contenido del dictionary: ");
+        foreach (var Alumno in casoDictionary.ListarAlumnos())
+        {
+            Console.WriteLine($"{Alumno.Key} -  {Alumno.Value.Nombre} |  Promedio {Alumno.Value.Promedio}");
+        }
+
+        Console.WriteLine("Buscando alumno por clave: 2");
+        Alumno?  busqueda = casoDictionary.BuscarAlumno(2);
+        if (busqueda != null)
+        {
+            Console.WriteLine($"Alumno encontrado: {busqueda.Id} - {busqueda.Nombre}| Promedio {busqueda.Promedio}");
+        }
+
+        Console.WriteLine("Buscando alumno que no existe: 934");
+        Alumno? busqueda2 = casoDictionary.BuscarAlumno(934);
+        if (busqueda2 == null)
+        {
+            Console.WriteLine("No existe");
+        }
+        
+        Console.WriteLine("Eliminando alumno y mostrando");
+        casoDictionary.EliminarAlumno(2);
+        foreach (var Alumno in casoDictionary.ListarAlumnos())
+        {
+            Console.WriteLine($"{Alumno.Key} - {Alumno.Value.Nombre} | Promedio {Alumno.Value.Promedio}");
+        }
 
     }
 
